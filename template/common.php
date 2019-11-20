98aa1e76cca81531692e1ee2fb550ab03a395872
Jose Gomes <zemiguelgomes99@gmail.com>
2019-11-20 10:18:43 +0000

    Started draw and organized folders

---
+++ template/common.php
@@ -0,0 +1,30 @@
+<!-- DRAW HEADER -->
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Botino</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans+Condensed:300" rel="stylesheet">
+</head>
+
+<body>
+
+  <div class="topnav">
+    <header>
+      <h1><a href=#HOMEPAGE></i> Botino</a></h1>
+    </header>
+    <input type="text" placeholder="Search..">
+  </div>
+
+
+  <section id="footer">
+  <!-- DRAW FOOTER -->
+  <a href="#about">About</a>
+  <a>|</a>
+  <a href="#contact">Contact</a>
+  </section>
+  </body>
+</html> 
