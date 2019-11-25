e6b1c6d37a5702b4661ac5bf4a595e2aee298d8a
José Gomes <zemiguelgomes99@gmail.com>
2019-11-25 19:17:34 +0000

    Added css and text

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ include_once('template/common.php');
 include_once('login.php');
 
 draw_header();
-draw_login();
+//draw_login();
 draw_search();
+draw_maintext();
 draw_footer();
 ?>
 
--- template/common.php
+++ template/common.php
@@ -9,24 +9,41 @@
     <title>Botino</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/style.css" />
+    <link href='https://fonts.googleapis.com/css?family=Black Han Sans rel='stylesheet'>
   </head>
 
   <body>
+    <div class="wrapper">
       <div class="topnav">
         <header>
           <h1><a href=#HOMEPAGE></i> Botino</a></h1>
         </header>
         <p> Slogan muito bonitão</p>
+        <hr>
       </div>
   <?php } ?>
+
+  <?php function draw_maintext()
+  {
+    ?>
+    <div class="descrip">
+     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae lacus tristique tortor semper porta. Phasellus vitae nulla vitae ex varius maximus. Mauris at pretium nulla. Vestibulum volutpat, ex et pretium aliquet, dui ex sodales dui, faucibus lobortis mauris neque eget lectus. Ut elementum ante massa. Nam ultrices fermentum lacus id pharetra. Nulla a purus at nibh viverra tempor. Mauris id sollicitudin tortor. Nullam aliquam ligula eu velit lacinia, vitae ullamcorper quam accumsan. </p>
+      </form>
+    </div>
+
+
+
+  <?php } ?>
+
+
 
   <?php function draw_search()
   {
     ?>
     <div class="searchbox">
       <h2>Search</h2>
-      <form method="post" action="#SEARCHPHP"> <br>
-        <label>Location</label>      
+      <form method="post" action="#SEARCHPHP">
+        <label>Location</label>
         <input type="text" name="location" placeholder="Where do you want to stay?" required><br>
         <label>Check-in</label> 
         <input type="date" name="checkin" placeholder="Check-in" required><br>
@@ -38,9 +55,6 @@
         <input type="submit" value="Search">
       </form>
     </div>
-
-
-
   <?php } ?>
 
 
@@ -50,7 +64,7 @@
     ?>
 
     <!-- DRAW FOOTER -->
-
+    </div>
     <section id="footer">
       <a href="#BecomeAHost">Rent your space</a>
       |
