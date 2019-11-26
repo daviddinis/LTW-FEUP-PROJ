a70bf1a9a6d87270ecbe1b5f9edc5e78818e0eed
José Gomes <zemiguelgomes99@gmail.com>
2019-11-26 16:19:40 +0000

    Started profile page

---
+++ template/tpl_profile.php
@@ -0,0 +1,31 @@
+<?php function draw_profile() //get_user(id) do url
+{?>
+    <div class="profile">
+    <img src="../generic-profile-pic.png" alt="Profile picture" width="160px" height="160px">
+    <h2>Name</h2>
+    <label>Antonio Gaiolo</label>
+    <h2>Username</h2>
+    <label>tonyFogueteiro</label>
+    <h2>Contacts</h2>
+    <label>Email: </label>antonio89@email.com<br>
+    <label>Phone: </label>999-999-999<br>
+
+
+    <h2>Reservation List</h2>
+    <p>aaaaa</p>
+    <p>aaaaa</p>
+    <p>aaaaa</p>
+    <p>aaaaa</p>
+    <p>aaaaa</p>
+    <p>aaaaa</p>
+    <p>aaaaa</p>
+    <p>aaaaa</p>
+    <p>aaaaa</p>
+    <p>aaaaa</p>
+    <p>aaaaa</p>
+    <p>aaaaa</p>
+    
+    
+    </div>
+<?php
+}?>
