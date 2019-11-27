f3c0648bf2b91683d80c0d34394b7d8c4b27461d
José Gomes <zemiguelgomes99@gmail.com>
2019-11-27 14:54:15 +0000

    Updated user page

--- template/tpl_profile.php
+++ template/tpl_profile.php
@@ -1,31 +1,38 @@
 <?php function draw_profile() //get_user(id) do url
 {?>
     <div class="profile">
-    <img src="../generic-profile-pic.png" alt="Profile picture" width="160px" height="160px">
-    <h2>Name</h2>
-    <label>Antonio Gaiolo</label>
-    <h2>Username</h2>
-    <label>tonyFogueteiro</label>
-    <h2>Contacts</h2>
-    <label>Email: </label>antonio89@email.com<br>
-    <label>Phone: </label>999-999-999<br>
+    <img src="../generic-profile-pic.png" alt="Profile picture" width="180px" height="180px">
+    <i class="material-icons">&#xe8b8;</i>
+        <div class="userInfo">
+            <h2>Name</h2>
+            <label>Antonio Gaiolo</label>
+            <h2>Username</h2>
+            <label>tonyFogueteiro</label>
+            <h2>Contacts</h2>
+            <label>Email: </label>antonio89@email.com<br>
+            <label>Phone: </label>999-999-999<br>
 
-
-    <h2>Reservation List</h2>
-    <p>aaaaa</p>
-    <p>aaaaa</p>
-    <p>aaaaa</p>
-    <p>aaaaa</p>
-    <p>aaaaa</p>
-    <p>aaaaa</p>
-    <p>aaaaa</p>
-    <p>aaaaa</p>
-    <p>aaaaa</p>
-    <p>aaaaa</p>
-    <p>aaaaa</p>
-    <p>aaaaa</p>
-    
-    
+            <h2>Reservation List</h2>
+            <ol>
+                <li>aaaaa</li>    
+                <li>aaaaa</li>
+                <li>aaaaa</li>
+                <li>aaaaa</li>
+                <li>aaaaa</li>
+                <li>aaaaa</li>
+                <li>aaaaa</li>  
+            </ol>
+        </div>
     </div>
 <?php
 }?>
+
+
+<?php function draw_() //get_user(id) do url
+{?>
+    <div class="profile">
+
+    </div>
+<?php
+}?>
+
