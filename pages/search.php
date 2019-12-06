50c637b5d9516fde878e790341596f9903b2d957
david <up201706766@fe.up.pt>
2019-12-06 14:19:44 +0000

    search begin not workin currently

---
+++ pages/search.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once '../includes/session.php';
+include_once '../template/tpl_common.php';
+include_once '../template/tpl_auth.php';
+
+draw_header();
+draw_login();
+draw_signUp();
+draw_search();
+draw_footer();
