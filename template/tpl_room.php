67064deb81a2f7b501e38137e6fab7771a2eea84
José Gomes <zemiguelgomes99@gmail.com>
2019-12-06 15:15:53 +0000

    Updated css

--- template/tpl_room.php
+++ template/tpl_room.php
@@ -9,14 +9,15 @@
         <label>Location</label>
         <input type="text" name="location" placeholder="Country,City,Street,HouseNº,PostalCode" required> <br>
         <label>Price per night</label>
-        <input type="number" name="price" placeholder="€€€" required><br>
+        <input type="number" name="price" step="0.1" placeholder="€€€" required><br>
+        <label>Number of guests</label>
+        <input type="number" name="guests" placeholder="Nº of guets" required><br>
         <label>Type of room</label>
         <input type="radio" name="type" value="House" checked> House
         <input type="radio" name="type" value="Apartment"> Apartment
         <input type="radio" name="type" value="other"> Other<br>
-        <label>Description</label>
-        <textarea name="message" rows="4" cols="30"> 
-        </textarea><br>
+        <label>Description</label><br>
+        <textarea name="message" rows="4" cols="30"></textarea><br>
         <input type="file" name="fileToUpload" id="fileToUpload" multiple><br> 
         <input id="searchbuttom" type="submit" value="Search">
     </form>
