a0beade6f4d4aea2d7e3ff3bbc3d796c553f7582
Mark Meehan <mtimothy38@gmail.com>
2019-12-16 21:11:27 +0000

    fixed issue with search returning multiple of the same places

--- database/db_search.php
+++ database/db_search.php
@@ -14,11 +14,12 @@ function search_database($location, $datein, $dateout) {
  
     $query_location = '%'.$location.'%';
  
-    $stmt = $db->prepare('SELECT * FROM reservation, place WHERE location LIKE ? and placeID=id EXCEPT SELECT * FROM reservation, place where ? BETWEEN checkIn and checkOut or ? BETWEEN checkIn and checkOut or ? <= checkIn and ? >= checkOut');
-
-
+    $stmt = $db->prepare('SELECT id, title, price, location, description, type, owner_username FROM reservation, place WHERE location LIKE ? and placeID=id EXCEPT SELECT id, title, price, location, description, type, owner_username FROM reservation, place where ? BETWEEN checkIn and checkOut or ? BETWEEN checkIn and checkOut or ? <= checkIn and ? >= checkOut');
+    // $stmt = $db->prepare('SELECT * FROM reservation, place WHERE location LIKE ? and placeID=id EXCEPT SELECT * FROM reservation, place where ? BETWEEN checkIn and checkOut or ? BETWEEN checkIn and checkOut or ? <= checkIn and ? >= checkOut');
+    
     $stmt->execute(array($query_location, $in, $out, $in, $out));
     // $stmt->execute(array($in, $out, $in, $out));
+
     return $stmt->fetchAll();
 }
 
