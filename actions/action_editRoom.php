93fc3440154215017ba9f657e4f50be286556c60
david <up201706766@fe.up.pt>
2019-12-17 16:20:29 +0000

    rent fix

--- actions/action_editRoom.php
+++ actions/action_editRoom.php
@@ -4,7 +4,7 @@ include_once '../database/db_upload.php';
 include_once '../database/db_room.php';
 
 $newRoom['id'] = $_GET['id'];
-$newRoom['title'] = $_GETz['title'];
+$newRoom['title'] = $_GET['title'];
 $newRoom['price'] = $_GET['price'];
 $newRoom['guests'] = $_GET['guests'];
 $newRoom['description'] = $_GET['description'];
--- actions/action_rent.php
+++ actions/action_rent.php
@@ -1,12 +1,12 @@
 <?php
-include_once('../includes/session.php');
-include_once('../database/db_reservation.php');
-include_once('../template/tpl_room.php');
+include_once '../includes/session.php';
+include_once '../database/db_reservation.php';
+include_once '../template/tpl_room.php';
 
 // Verify if user is logged in
-if (!isset($_SESSION['username']))
-  die(header('Location: ../pages/login.php'));
-
+if (!isset($_SESSION['username'])) {
+    die(header('Location: ../pages/login.php'));
+}
 
 $placeID = $_GET['id'];
 $checkin = $_GET['datein'];
@@ -18,9 +18,6 @@ $total_cost = $_GET['price'];
 // cenas($checkin);
 // cenas($checkout);
 
-
 add_reservation($placeID, $checkin, $checkout, $guests, $total_cost);
 
-header('Location: ../pages/user.php');
-
-?>
+header('Location: ../pages/login.php');
